79ce9df13e81106375440f20a72a0e1bbcb6d2ce
mhurtado13 <marcelo.hurt137@gmail.com>
2025-05-09 22:28:40 +0200

    Add astro

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://mhurtado.github.io',
+  base: 'my-repo',
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+  },
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+});
