1b08c2d44e31137eef30618f9370b3450bf2dd14
mhurtado13 <marcelo.hurt137@gmail.com>
2025-05-09 22:46:02 +0200

    Add config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mhurtado.github.io',
+  site: 'https://mhurtado13.github.io',
   base: 'my-repo',
   integrations: [tailwind(), react()],
   vite: {
